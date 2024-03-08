8033319456155fba9cb1e55e1195e80bcfd12c29
juan <juandtrez@gmail.com>
2024-03-08 15:57:06 -0500

    Ejercicio1:Defino propiedades junto a sus metodos setters y getters

--- EjercicioIntroJava01/Alumno.java
+++ EjercicioIntroJava01/Alumno.java
@@ -1,5 +1,47 @@
 package EjercicioIntroJava01;
 
+import java.util.ArrayList;
+
 public class Alumno {
+    private String nombre;
+    private String apellido;
+    private ArrayList<Asignatura> asignaturas;
     
-}
+    // Cosntructor de la clase alumno
+    public Alumno(String nombre, String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.asignaturas = new ArrayList<>();
+    }
+    
+    // Setter y getter para la variable nombre
+    public void Setnombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String Getnombre(){
+        return nombre;
+    }
+
+    // Setter y getter para la variable apellido
+    public void Setapellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    public String Getapellido(){
+        return apellido;
+    }
+
+    // Setter y getter para la variable asignatura
+    public ArrayList<Asignatura> getAsignaturas() {
+        return asignaturas;
+    }
+
+    public void setAsignaturas(ArrayList<Asignatura> asignaturas) {
+        this.asignaturas = asignaturas;
+    }
+
+    
+}  
+    
+
--- EjercicioIntroJava01/Asignatura.java
+++ EjercicioIntroJava01/Asignatura.java
@@ -1,5 +1,37 @@
 package EjercicioIntroJava01;
 
 public class Asignatura {
+    private String nombre;
+    private double nota;
+
+    //Cosntructor de la clase asignatura
+    public Asignatura (String nombre, double nota){
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    //Setter y getter para la variable nombre
+    public void Setnombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String Getnombre(){
+        return nombre;
+    }
+
+    //Setter y getter para la variable nota
+    public void Setnota(double nota){
+        this.nota = nota;
+    }
+
+    public double Getnota(){
+        return nota;
+    }
     
 }
+
+
+
+
+
+
