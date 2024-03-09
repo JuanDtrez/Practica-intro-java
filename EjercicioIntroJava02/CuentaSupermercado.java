291e421b5700ecea6f76452f77aa424a3ca6db34
juan <juandtrez@gmail.com>
2024-03-09 15:50:00 -0500

    Ejercicio2: Defino propiedades junto a sus metodos setters y getters

--- EjercicioIntroJava02/CuentaSupermercado.java
+++ EjercicioIntroJava02/CuentaSupermercado.java
@@ -1,5 +1,20 @@
 package EjercicioIntroJava02;
+import java.util.ArrayList;
 
 public class CuentaSupermercado {
-    
+    private ArrayList <Producto> productos;
+
+    // Constructor de la clase CuentaSupermercado
+    public CuentaSupermercado(){
+        this.productos = new ArrayList<>();
+    }
+
+    // Setter y getter para la lista productos
+    public void setProducto(ArrayList<Producto> productos){
+        this.productos = productos;
+    }
+
+    public ArrayList<Producto> getProducto(){
+        return productos;
+    }
 }
--- EjercicioIntroJava02/Producto.java
+++ EjercicioIntroJava02/Producto.java
@@ -1,5 +1,34 @@
 package EjercicioIntroJava02;
 
 public class Producto {
-    
+    // Clase que define el formato de un producto
+    private String nombre;
+    private double precioUnitario;
+
+    // Constructor de la clase producto
+    public Producto (String nombre, int precioUnitario){
+        this.nombre = nombre;
+        this.precioUnitario =precioUnitario;
+    }
+
+    // Setter y getter para la variable nombre
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    // Setter y getter para la variable precioUnitario
+    public void setprecioUnitario(double precioUnitario){
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getNota(){
+        return precioUnitario;
+    }
+
+
+
 }
